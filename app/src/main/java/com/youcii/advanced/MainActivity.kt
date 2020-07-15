ef321741dfa16ff86411a0f4e607a93580178c0b
jingdongwei <jingdongwei@58.com>
2020-07-15 16:26:39 +0800

    1. 系统悬浮窗

--- app/src/main/java/com/youcii/advanced/MainActivity.kt
+++ app/src/main/java/com/youcii/advanced/MainActivity.kt
@@ -1,11 +1,71 @@
-package com.youcii.advancedapp
+package com.youcii.advanced
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val OVERLAY_PERMISSION_REQ_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (textview.parent as View).setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (Settings.canDrawOverlays(this)) {
+                    //有悬浮窗权限开启服务绑定 绑定权限
+                    val intent = Intent(this, MyService::class.java)
+                    startService(intent)
+                } else {
+                    try {
+                        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+                        startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            } else {
+                // 默认有悬浮窗权限  但是 华为, 小米,oppo 等手机会有自己的一套 Android6.0 以下
+                // 会有自己的一套悬浮窗权限管理 也需要做适配
+                val intent = Intent(this, MyService::class.java)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(intent)
+                } else {
+                    startService(intent)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != OVERLAY_PERMISSION_REQ_CODE) {
+            return
+        }
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (Settings.canDrawOverlays(this)) {
+                //有悬浮窗权限开启服务绑定 绑定权限
+                val intent = Intent(this, MyService::class.java)
+                startService(intent)
+            } else {
+                Toast.makeText(this, "获取失败", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val intent = Intent(this, MyService::class.java)
+        stopService(intent)
     }
 }
---
+++ app/src/main/java/com/youcii/advanced/MyService.java
@@ -0,0 +1,57 @@
+package com.youcii.advanced;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    private WindowManager wManager;// 窗口管理者
+    private WindowManager.LayoutParams mParams;// 窗口的属性
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        wManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        mParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSPARENT);
+        // 系统提示window
+        if (Build.VERSION.SDK_INT >= 26) {
+            mParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            mParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        }
+
+        mParams.format = PixelFormat.TRANSLUCENT;// 支持透明
+        mParams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;// 焦点
+        mParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        mParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        mParams.gravity = Gravity.CENTER;
+        mParams.windowAnimations = android.R.style.Animation_Toast;
+        // mParams.alpha = 0.8f;//窗口的透明度
+
+
+        TextView textView = new TextView(this);
+        textView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText("阿诗丹顿多多多多多多多多多多多阿诗丹顿多多多多多多多多多多多阿诗丹顿多多多多多多多多多多多阿诗丹顿多多多多多多多多多多多阿诗丹顿多多多多多多多多多多多");
+        wManager.addView(textView, mParams);// 添加窗口
+
+    }
+
+}
