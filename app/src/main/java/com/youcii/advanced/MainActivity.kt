bf9f3e7c4be0633959e98115b8b43af789360ebf
jingdongwei <jingdongwei@58.com>
2021-10-12 15:41:42 +0800

    协程

--- app/src/main/java/com/youcii/advanced/MainActivity.kt
+++ app/src/main/java/com/youcii/advanced/MainActivity.kt
@@ -2,12 +2,26 @@ package com.youcii.advanced
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testCoroutines()
+    }
+
+    private fun testCoroutines() {
+
+    }
+
+    /**
+     * suspend 即挂起函数, 仅挂起协程, 不影响线程
+     */
+    private suspend fun fun1() {
+        delay(1000L)
     }
 
 }
