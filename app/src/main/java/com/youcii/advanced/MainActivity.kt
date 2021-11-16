b4f8a8c8211ce0b6044ce177657442eb0d47aab5
jingdongwei <jingdongwei@bytedance.com>
2021-11-16 17:36:44 +0800

    1. 第一步

--- app/src/main/java/com/youcii/advanced/MainActivity.kt
+++ app/src/main/java/com/youcii/advanced/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.youcii.advanced
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        entrance_lesson_list.layoutManager = LinearLayoutManager(this)
+        entrance_lesson_list.adapter = object: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, position: Int): RecyclerView.ViewHolder {
+                return object : RecyclerView.ViewHolder(TextView(this@MainActivity)) {
+                }
+            }
+
+            override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+                (viewHolder.itemView as TextView).text = position.toString()
+            }
+
+            override fun getItemCount(): Int {
+                return 100
+            }
+        }
     }
 
 }
